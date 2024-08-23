499fe45c47db8dd5d22aeaae6ee33ba4b4d8811b
Anas-kj <anaskhouajaa@gmail.com>
2024-08-23 11:59:53 +0800

    Update Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,48 @@
+import SearchForm from "@/components/SearchForm/SearchForm";
+import { trending_data } from "@/data/trending";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+    <main className="bg-[#003B95]">
+      <section className="p-6 max-w-7xl mx-auto">
+        <h2 className="text-white  leading-7 text-4xl lg:text-5xl font-semibold p-2">Find your Next Stay</h2>
+        <h3 className="text-white p-2 font-semibold lg:text-lg">
+            Search low prices on hotels, homes and much more... 
+        </h3>
+      </section>
+
+      <section className="m-4 mt-0 -mb-14 px-2 lg:px-4"> 
+        <SearchForm />
+      </section>
+
+      <section className="bg-white max-w-7xl mx-auto rounded-t-lg p-6 mt-10"> {/* Trending section */}
+        <div className="pt-5">
+          <h3 className="text-xl font-bold">Trending Destinations</h3>
+          <p className="font-light">
+            Most popular destinations in the world
+          </p>
+        </div>
+
+        <div className="flex space-x-4 py-5 overflow-x-scroll max-w-7xl mx-auto">
+          {trending_data.map((item) => (
+            <div key={item.id} className="flex flex-col items-start space-y-2 cursor-pointer shrink-0">
+              <div className="relative w-60 h-60 rounded-lg">
+                  <Image
+                    src={item.src}
+                    alt={item.title}  
+                    layout="fill"
+                    className="rounded-lg object-cover"              
+                  />
+              </div>
+                
+              <h3 className="font-semibold ">{item.title}</h3>
+              <p className="text-gray-500 text-sm">{item.location}</p>
+              <p className="font-light text-sm text-gray-500">{item.description}</p>
+            </div>
+          ))}
+        </div>
+      </section>
     </main>
   );
 }
---
+++ src/data/trending.ts
@@ -0,0 +1,37 @@
+export const trending_data = [
+    {
+      id: 1,
+      src: "https://images.unsplash.com/photo-1518684079-3c830dcef090?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8RHViYWl8ZW58MHx8MHx8fDA%3D",
+      title: "Dubai",
+      location: "United Arab Emirates",
+      description: "15 Deals",
+    },
+    {
+      id: 2,
+      src: "https://images.unsplash.com/photo-1570191913384-7b4ff11716e7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHNvdXRoJTIwa29yZWF8ZW58MHx8MHx8fDA%3D",
+      title: "South Korea",
+      location: "Asia",
+      description: "32 Deals",
+    },
+    {
+      id: 3,
+      src: "https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8TG9uZG9ufGVufDB8fDB8fHww",
+      title: "London",
+      location: "United Kingdom",
+      description: "45 Deals",
+    },
+    {
+      id: 4,
+      src: "https://images.unsplash.com/photo-1525625293386-3f8f99389edd?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2luZ2Fwb3JlfGVufDB8fDB8fHww",
+      title: "Singapore",
+      location: "Asia",
+      description: "88 Deals",
+    },
+    {
+      id: 5,
+      src: "https://images.unsplash.com/photo-1541336032412-2048a678540d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8TmV3JTIweW9ya3xlbnwwfHwwfHx8MA%3D%3D",
+      title: "New York City",
+      location: "United States of America",
+      description: "92 Deals",
+    },
+  ];
